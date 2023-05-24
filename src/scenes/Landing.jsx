c61d9f672c0bf4cbd8e421c799bb4356bcfd406c
Aryan Raj <aryanraj2k25@gmail.com>
2023-05-24 16:55:37 +0530

    social media icons added to the landing page

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -1,3 +1,4 @@
+import SocialMediaIcons from "../components/SocialMediaIcons";
 import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
@@ -95,6 +96,20 @@ const Landing=({setSelectedPage}) => {
                                     
                                 </AnchorLink>
 
+                            </motion.div>
+
+                            <motion.div
+                            className="flex mt-5 justify-center md:justify-start"
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true, amount: 0.5}}
+                            transition={{ delay:0.4, duration: 0.5}}
+                            variants={{
+                                hidden: {opacity: 0, x: -50},
+                                visible: {opacity: 1, x:0}
+                            }}
+                            >
+                                <SocialMediaIcons/>
                             </motion.div>
 
                     </div>
